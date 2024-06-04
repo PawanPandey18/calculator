72e6ef34211d277e71db0cddb747869988714965
PawanPandey18 <pandeypawan6541@gmail.com>
2024-06-04 13:51:48 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// let start="";
+// let i=0;
+// while (i<5) {
+//     let j=0;
+//     while (j<5-i) {
+//         start +=" ";
+//         j++
+//     }
+//     let k=0;
+//     while (k<=i) {
+//         start +="*";
+//         k++
+//     }
+    
+//     start+="\n";
+//     i++;
+    
+// }
+// console.log(start);
+
+//functions 
+  
+
+function Add() {
+    console.log(5+10);
+    
+}
+//Add ();
