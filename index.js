989f9a0d1ea59ec54839c2c390e1e5d3eede11e9
PawanPandey18 <pandeypawan6541@gmail.com>
2024-06-09 11:55:35 +0545

    second commit updating  functional calculater

--- index.js
+++ index.js
@@ -21,8 +21,68 @@
 //functions 
   
 
-function Add() {
-    console.log(5+10);
+// function Add() {
+//     console.log(5+10);
+    
+// }
+// Add ();
+let firstvalue=" ";
+let secondvalue="";
+let opt="";
+let result="";
+
+let display=document.getElementById('display')
+function oprt(b) {
+    opt=b;
+    display.value+=opt;
+
+}
+
+function Abc(a) {
+
+
+    
+    if (opt==''){
+        firstvalue+=a;
+        display.value=firstvalue
+
+    }
+    else{
+        secondvalue +=a;
+        display.value+=a
+
+    }
     
 }
-//Add ();
+
+function equals() {
+    let g=Number(firstvalue);
+    let h=Number(secondvalue);
+    if (opt=='+'){
+        result=g+h;
+        display.value+= '='+result;
+    }
+    else if (opt=='-'){
+        result=g-h;
+        display.value+='='+result;
+    }
+    else if (opt=='/'){
+        result=g/h;
+        display.value+='='+result;
+    }
+    else if (opt=="*"){
+        result=g*h;
+        display.value+='='+result;
+    }
+    else if(opt=='ac'){
+
+    }
+    else{
+        display.value=0;
+    }
+    
+}
+//const Abc =()=> console.log("hello")
+// const Abc=()=>(
+//     console.log("hello")
+// )
